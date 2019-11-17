3bb71ca9a12c25351a87db3e3a2d8bf70e7c26f7
rogojagad <rogojagadalit@gmail.com>
2019-11-17 12:51:00 +0700

    move key to .env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-require('dotenv').config()
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
