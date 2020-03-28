eb9e797de8810c1f2b525645e6ca156351d99ed1
rogojagad <rogojagadalit@gmail.com>
2020-03-28 11:06:28 +0700

    revise config for jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testEnvironment: "node",
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/tests/",
+        "/src/middleware/",
+        "/src/routes/"
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.{js,jsx}"]
+};
