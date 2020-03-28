a46ba83d5fec73dd533f0fb214f15ebdad069782
rogojagad <rogojagadalit@gmail.com>
2020-03-28 11:08:49 +0700

    add auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const responseCollection = require("./../responseStatus");
+
+exports.validateToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token === undefined)
+        return res.status(responseCollection.unauthorized).json({
+            message: "token not found",
+            name: "TokenNotFoundError"
+        });
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err)
+            return res.status(responseCollection.forbidden).json({
+                message: err.message,
+                name: err.name
+            });
+        req.user = user;
+        next();
+    });
+};
