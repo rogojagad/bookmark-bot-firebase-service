5df76114a95af2f984c30f5badd126524eab8164
rogojagad <rogojagadalit@gmail.com>
2021-02-11 18:27:38 +0700

    handle cors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
